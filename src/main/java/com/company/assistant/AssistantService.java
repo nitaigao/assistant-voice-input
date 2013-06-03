1ef5860ca5a14e789c46e2f656505eb062550230
Nicholas Kostelnik <nkostelnik@gmail.com>
2013-06-03 14:23:05 +0100

    first commit

---
+++ src/main/java/com/company/assistant/AssistantService.java
@@ -0,0 +1,5 @@
+package com.company.assistant;
+
+public class AssistantService {
+
+}
---
+++ src/main/java/com/company/assistant/EntryPoint.java
@@ -0,0 +1,9 @@
+package com.company.assistant;
+
+public class EntryPoint {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
